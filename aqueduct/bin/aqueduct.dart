a4e4c704db450fe0603fb61740a974086292add0
mufaddal1125 <mufaddal1125@gmail.com>
2021-02-08 12:54:39 +0400

    init

--- aqueduct/bin/aqueduct.dart
+++ aqueduct/bin/aqueduct.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:aqueduct/src/cli/runner.dart';
 
 Future main(List<String> args) async {
+  print('[Aqueduct mufaddal1125]');
   final runner = Runner();
   final values = runner.options.parse(args);
   exitCode = await runner.process(values);
